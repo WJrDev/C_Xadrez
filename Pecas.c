9cbce813ba15c80090c8cc0f6dfd801e0e6561c0
Zaqueu Noan <noanzaqueu@gmail.com>
2026-08-24 11:41:28 -0400

    Otimização do código
    Foi utilizado a estrutura de dados "Struct".

--- Pecas.c
+++ Pecas.c
@@ -1,15 +1,23 @@
 #include<stdio.h>
 #include<stdbool.h>
 
-int peca, direcaoVertical, direcaoHorizontal, lado, casas, indexDirecao = 1, maxCasas = 7;
-char* direcaoVerticalChar; 
-char* direcaoHorizontalChar;
+typedef struct {
+    int peca;
+    int lado;
+    int direcaoVertical;
+    int direcaoHorizontal;
+    int casas;
+    const char* direcaoVerticalChar;
+    const char* direcaoHorizontalChar;
+} EstadoJogo;
+
+EstadoJogo estado;
+int indexDirecao = 1, maxCasas = 7;
 
 bool EscolhaErrada(int escolha, int escolhaMax)
 {
     return escolha <= 0 || escolha > escolhaMax;
 }
-
 void EscolhaDePeca()
 {
     do
@@ -19,119 +27,107 @@ void EscolhaDePeca()
         printf("2. Bispo\n");
         printf("3. Rainha\n");
         printf("4. Cavalo\n");
-        scanf("%d", &peca);
+        scanf("%d", &estado.peca);
     }
-    while (EscolhaErrada(peca, 4));
-    
+    while (EscolhaErrada(estado.peca, 4));
 
-    switch (peca)
+    switch (estado.peca)
     {
         case 1:
         printf("Você escolheu a Torre\n");
         break;
-
         case 2:
         printf("Você escolheu o Bispo\n");
         break;
-    
+
         case 3:
         printf("Você escolheu a Rainha\n");
         break;
-
         case 4:
         printf("Você escolheu o Cavalo\n");
         break;
     }
 }
-
 void EscolhaDoLado()
 {
-    if(peca == 1)
+    if(estado.peca == 1)
     {
         do
         {
             printf("Para qual lado irá mover\n");
             printf("1. Cima ou Baixo\n");
             printf("2. Direita ou Esquerda\n");
-            scanf("%d", &lado);
-        } while (EscolhaErrada(lado, 2));
+            scanf("%d", &estado.lado);
+        } while (EscolhaErrada(estado.lado, 2));
     }
 }
-
 void EscolhaVertical()
 {
-    if(peca != 1 || lado == 1)
+    if(estado.peca != 1 || estado.lado == 1)
     {
         do
         {
             printf("Para onde irá mover verticalmente\n");
             printf("1. Cima\n");
             printf("2. Baixo\n");
-            scanf("%d", &direcaoVertical);
-        } while (EscolhaErrada(direcaoVertical, 2));
-    
-        if(direcaoVertical == 1) 
-            direcaoVerticalChar = "Cima\n";
-        else if(direcaoVertical == 2) 
-            direcaoVerticalChar = "Baixo\n";
+            scanf("%d", &estado.direcaoVertical);
+        } while (EscolhaErrada(estado.direcaoVertical, 2));
 
-        printf("Escolheu %s\n", direcaoVerticalChar);
+        if(estado.direcaoVertical == 1)
+            estado.direcaoVerticalChar = "Cima\n";
+        else if(estado.direcaoVertical == 2)
+            estado.direcaoVerticalChar = "Baixo\n";
+        printf("Escolheu %s\n", estado.direcaoVerticalChar);
     }
 }
-
 void EscolhaHorizontal()
 {
-    if(peca != 1 || lado == 2)
+    if(estado.peca != 1 || estado.lado == 2)
     {
         do
         {
             printf("Para onde irá mover horizontalmente\n");
             printf("1. Direita\n");
             printf("2. Esquerda\n");
-            scanf("%d", &direcaoHorizontal);
-        } while (EscolhaErrada(direcaoHorizontal, 2));
-    
-        if(direcaoHorizontal == 1) 
-            direcaoHorizontalChar = "Direita\n";
-        else if(direcaoHorizontal == 2) 
-            direcaoHorizontalChar = "Esquerda\n";
+            scanf("%d", &estado.direcaoHorizontal);
+        } while (EscolhaErrada(estado.direcaoHorizontal, 2));
+
+        if(estado.direcaoHorizontal == 1)
+            estado.direcaoHorizontalChar = "Direita\n";
+        else if(estado.direcaoHorizontal == 2)
+            estado.direcaoHorizontalChar = "Esquerda\n";
     }
 }
-
 void AndarNumeroDeCasas()
 {
-    switch (peca)
+    switch (estado.peca)
     {
         //Cavalo
         case 4:
-
         for(int i = 0; i < 2; i++)
         {
-            printf("%s", direcaoVerticalChar);
+            printf("%s", estado.direcaoVerticalChar);
         }
+        printf("%s", estado.direcaoHorizontalChar);
 
-        printf("%s", direcaoHorizontalChar);
-        
         break;
-    
+
         //Peças normais
         default:
         do
         {
             printf("Mova o número de casas que deseja de 1 até 7!\n");
-            scanf("%d", &casas);
-        } while (EscolhaErrada(casas, maxCasas));
-
-        for(int i = 0; i < casas; i++)
+            scanf("%d", &estado.casas);
+        } while (EscolhaErrada(estado.casas, maxCasas));
+        for(int i = 0; i < estado.casas; i++)
         {
-            if(direcaoHorizontal != 0) printf("%s", direcaoHorizontalChar);
-            if(direcaoVertical != 0) printf("%s", direcaoVerticalChar);
+            if(estado.direcaoHorizontal != 0) printf("%s", estado.direcaoHorizontalChar);
+            if(estado.direcaoVertical != 0) printf("%s", estado.direcaoVerticalChar);
             printf("\n");
         }
         break;
     }
 }
-
 int main()
 {
     EscolhaDePeca();
